8c16ab69d429bae0e2d6d4398a5900f467b12317
mrcreatist <ahk.verma102@gmail.com>
2017-11-14 23:39:29 +0530

    Commit from Android Studio

--- app/src/main/java/com/example/abhi/lifecalculator/MainActivity.java
+++ app/src/main/java/com/example/abhi/lifecalculator/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.abhi.lifecalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView name = (TextView) findViewById(R.id.name);
+                EditText getName = (EditText) findViewById(R.id.editText);
+
+                name.setText(getName.getText());
+            }
+        });
     }
 }
